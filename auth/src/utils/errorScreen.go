861965981090ea5a91b635ac7deea557213c9ac0
mattuu0 <tomatomato170@gmail.com>
2026-04-19 02:54:45 +0900

    refactor: エラー画面および認証テンプレートのパス解決とUIの改善

--- auth/src/utils/errorScreen.go
+++ auth/src/utils/errorScreen.go
@@ -6,15 +6,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func ErrorScreen(ctx echo.Context, code int, ErrorID string, Error error,IsPopup bool) error {
+func ErrorScreen(ctx echo.Context, code int, ErrorID string, Error error, IsPopup bool) error {
 	// エラー表示
-	logger.PrintErr2(2,"ErrorID: " + ErrorID,Error)
+	logger.PrintErr2(2, "ErrorID: "+ErrorID, Error)
 
 	return ctx.Render(code, "error-screen.html", echo.Map{
-		"StatusCode": code,
-		"ErrorID":    ErrorID,
-		"Error":      Error.Error(),
-		"isPopup":    BoolToInt(IsPopup),
+		"StatusCode":   code,
+		"ErrorID":      ErrorID,
+		"Error":        Error.Error(),
+		"isPopup":      BoolToInt(IsPopup),
+		"RelativePath": "../../",
 	})
 }
 
