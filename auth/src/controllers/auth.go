7d479ca679c09e817dfc5f0a3eefd8a223c38fc2
mattuu0 <tomatomato170@gmail.com>
2026-04-19 05:19:15 +0900

    chore: auth.go の余計な空白を削除

--- auth/src/controllers/auth.go
+++ auth/src/controllers/auth.go
@@ -9,38 +9,21 @@ import (
 )
 
 func Logout(ctx echo.Context) error {
-
 	// セッションを取得
-
 	session, ok := ctx.Get("session").(*models.Session)
 
-
-
 	// エラー処理
-
 	if !ok {
-
 		return ctx.JSON(http.StatusUnauthorized, echo.Map{"error": "unauthorized"})
-
 	}
-
-
 
 	// セッションを削除
-
 	if err := services.Logout(session); err != nil {
-
 		return ctx.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
-
 	}
 
-
-
 	return ctx.JSON(http.StatusOK, echo.Map{"message": "success"})
-
 }
-
-
 
 func LoginPage(ctx echo.Context) error {
 	isPopup := ctx.QueryParam("ispopup")
